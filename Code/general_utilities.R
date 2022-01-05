61e0ee0db57c17f2a8b0c8fa1970fb3495d7cc1e
mceverett8 <michael.c.everett.19@gmail.com>
2022-01-04 16:17:21 -0800

    Update to read in UCLA decedent data and compare with BJS

--- Code/general_utilities.R
+++ Code/general_utilities.R
@@ -1,45 +1,231 @@
-## UCLA Prison Mortality Utilities
+#### UCLA Prison Mortality Utilities
 
-## Set Environment
+#### Set Environment -----------------------------------
 library(tidyverse)
 library(httr)
 library(magrittr)
 library(reshape2)
 library(lubridate)
+library(data.table)
 
-## Functions
+#### Functions -----------------------------------------
 
-# Read in BJS Historical Decedent Data (COMPLETE)
+## (1) Read in BJS Historical Decedent Data ------------
 
 read_bjs <- function(all.agencies, agencies) {
-    mci.19 <- 'Data/External/msfp0119stt14.csv' %>%
+    suppressMessages(suppressWarnings(mci.19 <- 'Data/External/msfp0119stt14.csv' %>%
                read_csv() %>%
                subset(!is.na(X3)) %>%
                mutate(Jurisdiction = coalesce(`Bureau of Justice Statistics`, X2)) %>%
                select(-c(`Bureau of Justice Statistics`, X2))
+    ))
     mci.cols <- mci.19[1,]
-    mci.19 <- mci.19 %>%
+    suppressMessages(suppressWarnings(mci.19 <- mci.19 %>%
               set_colnames(mci.cols) %>%
               melt() %>%
               mutate(Jurisdiction = str_replace_all(Jurisdiction, '\\/.*', '')) %>%
               subset(!str_detect(Jurisdiction, 'Jurisdiction') & !str_detect(Jurisdiction, 'State')) %>%
               set_colnames(c('Jurisdiction', 'Year', 'Total.Deaths'))
-    
-    if(all.agencies == FALSE) {
-        mci.19
-    }
+    ))
     
     if(all.agencies == TRUE) {
+        print('Pulling all annual agency data from BJS')
+    }
+    
+    if(all.agencies == FALSE) {
         mci.19 <- mci.19 %>%
             subset(Jurisdiction %in% agencies)
-        mci.19
+        print('Pulling specific agency data from BJS for:')
     }
+    
+    mci.19 <- mci.19 %>%
+              plyr::rename(c('Jurisdiction' = 'State')) %>%
+              as.data.frame()
+    mci.19$Year <- as.numeric(as.character(mci.19$Year))
+    mci.19
     
 }
 
-# Read in UCLA Historical Decedent Data (WORKING)
+# (2) Read in UCLA Historical Decedent Data -------------------------
+
+# sub functions for read_ucla_data
+
+# pull raw files
+
+pull_raw_files <- function(path) {
+    out <- 'Data/Raw/' %>%
+        str_c(., path) %>%
+        list.files() %>%
+        as.data.frame() %>%
+        set_colnames('Files')
+    out
+}
+
+# make year aggregate function
+agg.year <- function(x) {
+    read <- x %>%
+        read.csv() 
+    data.cols <- read %>%
+        colnames() %>%
+        as.vector()
+    if(!('Total.Deaths' %in% data.cols)) {
+        out <- read %>%
+            group_by(State, Year) %>%
+            summarise(Total.Deaths = n())
+    }
+    
+    if('Total.Deaths' %in% data.cols) {
+        out <- read %>%
+            group_by(State, Year) %>%
+            summarise(Total.Deaths = sum(Total.Deaths, na.rm = TRUE))
+    }
+    out
+}
+
+# all var sum to year obs
+sum.to.year <- function(x) {
+    read <- x %>%
+        read.csv() 
+    data.cols <- read %>%
+        colnames() %>%
+        as.vector()
+    group.cols <- data.cols %>%
+        as.data.frame() %>%
+        plyr::rename(c('.' = 'cols')) %>%
+        subset(cols != 'Month' & cols != 'Total.Deaths' & cols != 'Death.Date') 
+    group.cols <- group.cols$cols 
+    if(!('Total.Deaths' %in% data.cols)) {
+        out <- read %>%
+            group_by(across(all_of(group.cols))) %>%
+            summarise(Total.Deaths = n())
+    }
+    
+    if('Total.Deaths' %in% data.cols) {
+        out <- read %>%
+            group_by(across(all_of(group.cols))) %>%
+            summarise(Total.Deaths = sum(Total.Deaths, na.rm = TRUE))
+            
+    }
+    out
+}
+
+# all var sum to month obs
+sum.to.month <- function(x) {
+    read <- x %>%
+        read.csv() 
+    data.cols <- read %>%
+        colnames() %>%
+        as.vector()
+    group.cols <- data.cols %>%
+        as.data.frame() %>%
+        plyr::rename(c('.' = 'cols')) %>%
+        subset(cols != 'Total.Deaths' & cols != 'Death.Date') 
+    group.cols <- group.cols$cols 
+    if(!('Total.Deaths' %in% data.cols)) {
+        out <- read %>%
+            group_by(across(all_of(group.cols))) %>%
+            summarise(Total.Deaths = n())
+    }
+    
+    if('Total.Deaths' %in% data.cols) {
+        out <- read %>%
+            group_by(across(all_of(group.cols))) %>%
+            summarise(Total.Deaths = sum(Total.Deaths, na.rm = TRUE))
+        
+    }
+    out
+}
+
+# all var sum to individual obs
+sum.to.individual <- function(x) {
+    read <- x %>%
+        read.csv() 
+    data.cols <- read %>%
+        colnames() %>%
+        as.vector()
+    group.cols <- data.cols %>%
+        as.data.frame() %>%
+        plyr::rename(c('.' = 'cols')) %>%
+        subset(cols != 'Total.Deaths') 
+    group.cols <- group.cols$cols 
+    if(!('Total.Deaths' %in% data.cols)) {
+        out <- read %>%
+            group_by(across(all_of(group.cols))) %>%
+            summarise(Total.Deaths = n())
+    }
+    
+    if('Total.Deaths' %in% data.cols) {
+        out <- read %>%
+            group_by(across(all_of(group.cols))) %>%
+            summarise(Total.Deaths = sum(Total.Deaths, na.rm = TRUE))
+        
+    }
+    out
+}
+
+# Make month to year function
+month.to.year <- function(x) {
+    read <- x %>%
+        read.csv() 
+    data.cols <- read %>%
+        colnames() %>%
+        as.vector()
+    if(!('Total.Deaths' %in% data.cols)) {
+        out <- read %>%
+            group_by(State, Year) %>%
+            summarise(Total.Deaths = n())
+    }
+    
+    if('Total.Deaths' %in% data.cols) {
+        out <- read %>%
+            group_by(State, Year) %>%
+            summarise(Total.Deaths = sum(Total.Deaths))
+    }
+    out
+}
+
+# Make individual to year function
+individual.to.year <- function(x) {
+    read <- x %>%
+        read.csv() %>%
+        group_by(State, Year) %>%
+        summarise(Total.Deaths = n())
+}
+
+# Read to Year aggregate function (default operation)
+read_to_year <- function(file.base) {
+    file.list <- file.base %>%
+                 select(Files, Data.Type) %>%
+                 mutate(Files = str_c('Data/Raw/Deaths/', Data.Type, '/', Files)) %>%
+                 subset(!str_detect(Files, 'UT-Monthly')) # remove more detailed UT data for yearly based aggregate
+    # Prepare Annual Data
+    year.data <- file.list %>%
+        subset(Data.Type == 'Annual')
+    # end year aggregate function
+    year.list <- lapply(year.data$Files, agg.year)
+    year.out <- rbindlist(year.list, fill = TRUE)
+    # Prepare Monthly Data
+    month.data <- file.list %>%
+        subset(Data.Type == 'Monthly')
+
+    month.list <- lapply(month.data$Files, month.to.year)
+    month.out <- rbindlist(month.list, fill = TRUE)
+    # Prepare Individual Data
+    individual.data <- file.list %>%
+        subset(Data.Type == 'Individual')
+    individual.list <- lapply(individual.data$Files, individual.to.year)
+    individual.out <- rbindlist(individual.list, fill = TRUE)
+    
+    all.deaths <- year.out %>%
+        plyr::rbind.fill(month.out) %>%
+        plyr::rbind.fill(individual.out)
+    all.deaths
+}
+
 
 read_ucla_deaths <- function(all.agencies, agencies) {
+    
+    ## Pull all possible files in repo and set up state dataframe
     states <- data.frame(State.Abb = state.abb,
                          State.Name = state.name)
     annual.files <- 'Deaths/Annual' %>%
@@ -57,39 +243,104 @@ read_ucla_deaths <- function(all.agencies, agencies) {
                         Data.Type = ifelse(str_detect(Data.Type, 'Yearly'), 'Annual', Data.Type)) %>%
                  left_join(., states, by = c('State.Abb')) 
     
-    if(all.agencies = TRUE) {
-        file.list <- all.files %>%
-                     select(Files, Data.Type) %>%
-                     mutate(Files = str_c('Data/Raw/Deaths/', Data.Type, '/', Files)) %>%
-                     select(Files) 
-        file.list <- file.list$Files
-        death.list <- lapply(file.list, read.csv)
+    # User wants all potential decedent data
+    if(all.agencies == TRUE) {
+    suppressMessages(output.deaths <- all.files %>%
+                     read_to_year())
+    print('Summarized by years of data available for all agencies from UCLA')
     }
     
-    if(all.agencies = FALSE) {
-        if(str_detect()) {}
+    # User wants decedent data from specific states
+    # If user wants states of different data levels (i.e. Annual, Monthly, Individual)
+    if(all.agencies == FALSE) {
+        # function testing
+        #agencies <- c('MN', 'AR', 'GA', 'NV', 'NC')
+        # set selected states from user
+        input <- agencies
+        # create file frame of selected states
+        file.list <- all.files %>%
+            select(Files, Data.Type) %>%
+            mutate(Files = str_c('Data/Raw/Deaths/', Data.Type, '/', Files),
+                   State = str_replace_all(Files, '-.*', ''),
+                   State = str_replace_all(State, 'Data\\/Raw\\/Deaths\\/', ''),
+                   State = str_replace_all(State, '.*\\/', '')) %>%
+            subset(State %in% input)
+        ## Check for levels in data to aggregate to same level
+        # For all three levels
+        suppressMessages(
+        if(('Annual' %in% file.list$Data.Type) &
+           ('Monthly' %in% file.list$Data.Type) &
+           ('Individual' %in% file.list$Data.Type)) {
+           output.list <- lapply(file.list$Files, sum.to.year)
+           output.deaths <- rbindlist(output.list, fill = TRUE)
+           print('Aggregated annually')
+        })
+        # For yearly only
+        suppressMessages(
+        if(('Annual' %in% file.list$Data.Type) &
+           !('Monthly' %in% file.list$Data.Type) &
+           !('Individual' %in% file.list$Data.Type)) {
+            output.list <- lapply(file.list$Files, sum.to.year)
+            output.deaths <- rbindlist(output.list, fill = TRUE)
+            print('Aggregated annually')
+        })
+        # For monthly only
+        suppressMessages(
+        if(!('Annual' %in% file.list$Data.Type) &
+           ('Monthly' %in% file.list$Data.Type) &
+           !('Individual' %in% file.list$Data.Type)) {
+            output.list <- lapply(file.list$Files, sum.to.month)
+            output.deaths <- rbindlist(output.list, fill = TRUE)
+            print('Aggregated monthly')
+        })
+        # For individual only
+        suppressMessages(
+        if(!('Annual' %in% file.list$Data.Type) &
+           !('Monthly' %in% file.list$Data.Type) &
+           ('Individual' %in% file.list$Data.Type)) {
+            output.list <- lapply(file.list$Files, read.csv)
+            output.deaths <- rbindlist(output.list, fill = TRUE)
+            print('All individual level, no aggregation')
+        })
+        # For yearly and monthly
+        suppressMessages(
+        if(('Annual' %in% file.list$Data.Type) &
+           ('Monthly' %in% file.list$Data.Type) &
+           !('Individual' %in% file.list$Data.Type)){
+            output.list <- lapply(file.list$Files, sum.to.year)
+            output.deaths <- rbindlist(output.list, fill = TRUE)
+            print('Aggregated annually')
+        })
+        # For monthly and individual
+        suppressMessages(
+        if(!('Annual' %in% file.list$Data.Type) &
+           ('Monthly' %in% file.list$Data.Type) &
+           ('Individual' %in% file.list$Data.Type)) {
+            output.list <- lapply(file.list$Files, sum.to.month)
+            output.deaths <- rbindlist(output.list, fill = TRUE)
+            print('Aggregated monthly')
+        })
     }
+    output.deaths
 }
-
-pull_raw_files <- function(path) {
-    out <- 'Data/Raw/' %>%
-            str_c(., path) %>%
-            list.files() %>%
-            as.data.frame() %>%
-            set_colnames('Files')
-    out
-}
-
-# Sub function to check variables present in decedent data to determine level of end aggregation (WORKING)
-
-check_death_lists <- function(death.list) {}
     
 # Compare Annual BJS Numbers with Aggregated UCLA numbers (WORKING)
 
-compare_ucla_bjs <- function(all.agencies) {}
-
-
-
+compare_ucla_bjs <- function() {
+    bjs.data <- read_bjs(all.agencies = TRUE) 
+    bjs.data <- bjs.data %>%
+                plyr::rename(c('Total.Deaths' = 'BJS.Deaths'))
+    ucla.data <- read_ucla_deaths(all.agencies = TRUE) 
+    ucla.data <- ucla.data %>%
+                 plyr::rename(c('Total.Deaths' = 'UCLA.Deaths'))
+    joined.data <- ucla.data %>%
+                   left_join(., bjs.data, by = c('State', 'Year')) %>%
+                   mutate(Absolute.Difference = abs(UCLA.Deaths-BJS.Deaths)) %>%
+                   subset(Year < 2021) %>%
+                   arrange(desc(Absolute.Difference))
+    joined.data
+    
+}
 
 
 
